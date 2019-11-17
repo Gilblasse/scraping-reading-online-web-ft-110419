a48db0cf7c74255b33f2a1310e49c817a6482726
Nethelbert <gilblasse@gmail.com>
2019-11-17 23:53:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+flatirons_header = doc.css("#header-content > div > h1").text
+
+binding.pry
