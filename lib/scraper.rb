2ac37c5fae43c6a152a67f1303ff42074a16a1ce
Nethelbert <gilblasse@gmail.com>
2019-11-18 01:27:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,8 @@ doc = Nokogiri::HTML(html)
 
 flatirons_header = doc.css("#header-content > div > h1").text
 
-career_section = doc.xpath('')
-courses = career_section.search(".title-oE5vT4").map {|title| title.text}
-paragraphs = career_section.search("p").map {|content| content.text}
+career_section = doc.xpath('.content-3OTK_R')
+# courses = career_section.search(".title-oE5vT4").map {|title| title.text}
+# paragraphs = career_section.search("p").map {|content| content.text}
 
-courses_section = courses.zip(paragraphs)
 binding.pry
