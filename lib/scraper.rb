3e7956d2a347c9e9d162c8544a6c5ca6b01bbf1b
Nethelbert <gilblasse@gmail.com>
2019-11-18 00:58:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,4 +7,4 @@ doc = Nokogiri::HTML(html)
 
 flatirons_header = doc.css("#header-content > div > h1").text
 
-doc.xpath('//*[@id="2a778efd-1685-5ec6-9e5a-0843d6a88b7b"]/div/div[2]/div/div[1]/div[2]/div[1]')
+path = doc.xpath('//*[@id="2a778efd-1685-5ec6-9e5a-0843d6a88b7b"]/div/div[2]/div/div[1]/div[2]/div[1]')
