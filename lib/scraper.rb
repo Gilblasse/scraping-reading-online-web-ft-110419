625fb6d281fecdff5aed34675997fdd0254a7552
Nethelbert <gilblasse@gmail.com>
2019-11-18 01:41:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,4 +9,4 @@ flatirons_header = doc.css("#header-content > div > h1").text
 
 courses = doc.search('#\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div .title-oE5vT4').map {|title| title.text}
 puts courses
-binding.pry
+
