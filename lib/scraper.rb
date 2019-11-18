bdcbd2374d05f85084897e9c4ea099fb70c90f05
Nethelbert <gilblasse@gmail.com>
2019-11-18 00:35:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ doc = Nokogiri::HTML(html)
 flatirons_header = doc.css("#header-content > div > h1").text
 doc.search(".title-oE5vT4")
 
-courses = doc.css(".title-oE5vT4").text
+courses = doc.css(".title-oE5vT4")
 
 courses.each {|course| puts course}
   
