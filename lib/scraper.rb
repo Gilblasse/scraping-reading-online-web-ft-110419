cf92ff821d85bb3307e5abbd56f9acbca48b9db1
Nethelbert <gilblasse@gmail.com>
2019-11-18 01:34:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(html)
 
 flatirons_header = doc.css("#header-content > div > h1").text
 
-career_section = doc.search('.gridContainer-EnvccM')
+career_section = doc.xpath('//*[@id="2a778efd-1685-5ec6-9e5a-0843d6a88b7b"]/div/div[2]/div')
 # courses = career_section.search(".title-oE5vT4").map {|title| title.text}
 # paragraphs = career_section.search("p").map {|content| content.text}
 
