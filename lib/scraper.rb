4fd57884975f16e7d02ffb7ce4b309558afbc5a5
Nethelbert <gilblasse@gmail.com>
2019-11-18 00:33:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,5 +8,5 @@ doc = Nokogiri::HTML(html)
 flatirons_header = doc.css("#header-content > div > h1").text
 doc.search(".title-oE5vT4")
 
-# document.querySelector("#\\32 a778efd-1685-5ec6-9e5a-0843d6a88b7b > div > div.content-3OTK_R > div > div:nth-child(1) > div.text-4GLMvr > div.title-oE5vT4")
+courses = doc.css(".title-oE5vT4")
 binding.pry
